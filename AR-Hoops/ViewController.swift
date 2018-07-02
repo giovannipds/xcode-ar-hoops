8344266bae246368d1298524f26bf1bf570d6a97
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-07-02 02:39:46 -0300

    Add Each pod and make a logic to hold the touch and increase the ball's force (though it doesn't work again)

--- AR-Hoops/ViewController.swift
+++ AR-Hoops/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import ARKit
+import Each
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var planeDetected: UILabel!
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
-    var power: Float = 1.0
+    var power: Float = 1
+    let timer = Each(0.05).seconds
     var basketAdded: Bool {
         return self.sceneView.scene.rootNode.childNode(withName: "Basket", recursively: false) != nil
     }
@@ -31,21 +33,36 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if self.basketAdded == true {
-            guard let pointOfView = self.sceneView.pointOfView else {return}
-            self.power = 10
-            let transform = pointOfView.transform
-            let location = SCNVector3(transform.m41, transform.m42, transform.m43)
-            let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
-            let position = location + orientation
-            let ball = SCNNode(geometry: SCNSphere(radius: 0.3))
-            ball.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "ball")
-            ball.position = position
-            let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: ball))
-            ball.physicsBody = body
-            
-            ball.physicsBody?.applyForce(SCNVector3(orientation.x*power, orientation.y*power, orientation.z*power), asImpulse: true)
-            self.sceneView.scene.rootNode.addChildNode(ball)
+            timer.perform(closure: { () -> NextStep in
+                self.power = self.power + 1
+                return .continue
+            })
         }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if self.basketAdded == true {
+            self.timer.stop()
+            self.shootBall()
+        }
+        self.power = 1
+    }
+    
+    func shootBall() {
+        guard let pointOfView = self.sceneView.pointOfView else {return}
+        self.power = 10
+        let transform = pointOfView.transform
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+        let position = location + orientation
+        let ball = SCNNode(geometry: SCNSphere(radius: 0.25))
+        ball.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "ball")
+        ball.position = position
+        let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: ball))
+        ball.physicsBody = body
+        
+        ball.physicsBody?.applyForce(SCNVector3(orientation.x*power, orientation.y*power, orientation.z*power), asImpulse: true)
+        self.sceneView.scene.rootNode.addChildNode(ball)
     }
     
     @objc func handleTap(sender: UITapGestureRecognizer) {
@@ -83,6 +100,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             self.planeDetected.isHidden = true
         }
+    }
+    deinit {
+        self.timer.stop()
     }
 
 }
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AR-Hoops' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AR-Hoops
+
+  target 'AR-HoopsTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AR-HoopsUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+
+pod 'Each', '~> 1.2'
+
+end
